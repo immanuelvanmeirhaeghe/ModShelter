8c5e15dfc0c6f150b2244686e7e53ed54569a623
Immanuel Van Meirhaeghe <immanuelvm@live.be>
2020-12-04 19:16:26 +0100

    472+ui

--- ModShelter.cs
+++ ModShelter.cs
@@ -6,6 +6,13 @@ using UnityEngine;
 
 namespace ModShelter
 {
+    public enum MessageType
+    {
+        Info,
+        Warning,
+        Error
+    }
+
     /// <summary>
     /// ModShelter is a mod for Green Hell
     /// that allows a player to unlocks all shelters and beds.
@@ -15,32 +22,80 @@ namespace ModShelter
     /// </summary>
     public class ModShelter : MonoBehaviour
     {
-        private static ModShelter s_Instance;
-
         private static readonly string ModName = nameof(ModShelter);
+        private static readonly float ModScreenTotalWidth = 500f;
+        private static readonly float ModScreenTotalHeight = 150f;
+        private static readonly float ModScreenMinWidth = 450f;
+        private static readonly float ModScreenMaxWidth = 550f;
+        private static readonly float ModScreenMinHeight = 50f;
+        private static readonly float ModScreenMaxHeight = 200f;
+
+        private static ModShelter Instance;
+        private static ItemsManager LocalItemsManager;
+        private static HUDManager LocalHUDManager;
+        private static Player LocalPlayer;
+
+        private static float ModScreenStartPositionX { get; set; } = (Screen.width - ModScreenMaxWidth) % ModScreenTotalWidth;
+        private static float ModScreenStartPositionY { get; set; } = (Screen.height - ModScreenMaxHeight) % ModScreenTotalHeight;
+        private static bool IsMinimized { get; set; } = false;
 
         private bool ShowUI = false;
 
-        public static Rect ModShelterScreen = new Rect(Screen.width /10f, Screen.height / 2f, 450f, 150f);
+        public static Rect ModShelterScreen = new Rect(ModScreenStartPositionX, ModScreenStartPositionY, ModScreenTotalWidth, ModScreenTotalHeight);
 
-        private static ItemsManager itemsManager;
+        public static List<ItemID> ShelterItemIds { get; set; } = new List<ItemID> {
+            ItemID.Bed_Shelter,
+            ItemID.Hut_Shelter,
+            ItemID.Medium_Shelter,
+            ItemID.Medium_Bamboo_Shelter,
+            ItemID.Small_Shelter,
+            ItemID.Small_Bamboo_Shelter
+        };
 
-        private static HUDManager hUDManager;
-
-        private static Player player;
-
-        public static List<ItemID> ShelterItemIds { get; set; } = new List<ItemID> { ItemID.Hut_Shelter, ItemID.Medium_Bamboo_Shelter, ItemID.Medium_Shelter, ItemID.Small_Bamboo_Shelter, ItemID.Bed_Shelter };
-
-        public static List<ItemID> BedItemIds { get; set; } = new List<ItemID> { ItemID.Leaves_Bed, ItemID.banana_leaf_bed, ItemID.Logs_Bed, ItemID.BambooLog_Bed };
-
+        public static List<ItemID> BedItemIds { get; set; } = new List<ItemID> {
+            ItemID.Leaves_Bed,
+            ItemID.banana_leaf_bed,
+            ItemID.Logs_Bed,
+            ItemID.BambooLog_Bed
+        };
         public static List<ItemInfo> RestingPlaceItemInfos = new List<ItemInfo>();
-        public static bool HasUnlockedRestingPlaces { get; set; }
-        public bool InstantFinishConstructionsOption { get; private set; }
 
-        public bool IsModActiveForMultiplayer { get; private set; }
+        public bool HasUnlockedRestingPlaces { get; set; } = false;
+        public bool InstantFinishConstructionsOption { get; private set; } = false;
+        public bool IsModActiveForMultiplayer { get; private set; } = false;
         public bool IsModActiveForSingleplayer => ReplTools.AmIMaster();
 
-        private static string HUDBigInfoMessage(string message) => $"<color=#{ColorUtility.ToHtmlStringRGBA(Color.red)}>System</color>\n{message}";
+        public static string OnlyForSinglePlayerOrHostMessage() => $"Only available for single player or when host. Host can activate using ModManager.";
+        public static string AlreadyUnlockedBlueprints() => $"All blueprints were already unlocked!";
+        public static string PermissionChangedMessage(string permission) => $"Permission to use mods and cheats in multiplayer was {permission}!";
+        public static string HUDBigInfoMessage(string message, MessageType messageType, Color? headcolor = null)
+            => $"<color=#{ (headcolor != null ? ColorUtility.ToHtmlStringRGBA(headcolor.Value) : ColorUtility.ToHtmlStringRGBA(Color.red))  }>{messageType}</color>\n{message}";
+
+        public void ShowHUDBigInfo(string text)
+        {
+            string header = $"{ModName} Info";
+            string textureName = HUDInfoLogTextureType.Count.ToString();
+            HUDBigInfo hudBigInfo = (HUDBigInfo)LocalHUDManager.GetHUD(typeof(HUDBigInfo));
+            HUDBigInfoData.s_Duration = 2f;
+            HUDBigInfoData hudBigInfoData = new HUDBigInfoData
+            {
+                m_Header = header,
+                m_Text = text,
+                m_TextureName = textureName,
+                m_ShowTime = Time.time
+            };
+            hudBigInfo.AddInfo(hudBigInfoData);
+            hudBigInfo.Show(true);
+        }
+
+        public void ShowHUDInfoLog(string itemID, string localizedTextKey)
+        {
+            var localization = GreenHellGame.Instance.GetLocalization();
+            HUDMessages hUDMessages = (HUDMessages)LocalHUDManager.GetHUD(typeof(HUDMessages));
+            hUDMessages.AddMessage(
+                $"{localization.Get(localizedTextKey)}  {localization.Get(itemID)}"
+                );
+        }
 
         public void Start()
         {
@@ -52,44 +107,20 @@ namespace ModShelter
             IsModActiveForMultiplayer = optionValue;
             ShowHUDBigInfo(
                           (optionValue ?
-                            HUDBigInfoMessage($"<color=#{ColorUtility.ToHtmlStringRGBA(Color.green)}>Permission to use mods for multiplayer was granted!</color>")
-                            : HUDBigInfoMessage($"<color=#{ColorUtility.ToHtmlStringRGBA(Color.yellow)}>Permission to use mods for multiplayer was revoked!</color>")),
-                           $"{ModName} Info",
-                           HUDInfoLogTextureType.Count.ToString());
+                            HUDBigInfoMessage(PermissionChangedMessage($"granted"), MessageType.Info, Color.green)
+                            : HUDBigInfoMessage(PermissionChangedMessage($"revoked"), MessageType.Info, Color.yellow))
+                            );
         }
 
         public ModShelter()
         {
             useGUILayout = true;
-            s_Instance = this;
+            Instance = this;
         }
 
         public static ModShelter Get()
         {
-            return s_Instance;
-        }
-
-        public void ShowHUDBigInfo(string text, string header, string textureName)
-        {
-            HUDBigInfo bigInfo = (HUDBigInfo)hUDManager.GetHUD(typeof(HUDBigInfo));
-            HUDBigInfoData bigInfoData = new HUDBigInfoData
-            {
-                m_Header = header,
-                m_Text = text,
-                m_TextureName = textureName,
-                m_ShowTime = Time.time
-            };
-            bigInfo.AddInfo(bigInfoData);
-            bigInfo.Show(true);
-        }
-
-        public void ShowHUDInfoLog(string itemID, string localizedTextKey)
-        {
-            var localization = GreenHellGame.Instance.GetLocalization();
-            HUDMessages hUDMessages = (HUDMessages)hUDManager.GetHUD(typeof(HUDMessages));
-            hUDMessages.AddMessage(
-                $"{localization.Get(localizedTextKey)}  {localization.Get(itemID)}"
-                );
+            return Instance;
         }
 
         private void EnableCursor(bool blockPlayer = false)
@@ -98,15 +129,15 @@ namespace ModShelter
 
             if (blockPlayer)
             {
-                player.BlockMoves();
-                player.BlockRotation();
-                player.BlockInspection();
+                LocalPlayer.BlockMoves();
+                LocalPlayer.BlockRotation();
+                LocalPlayer.BlockInspection();
             }
             else
             {
-                player.UnblockMoves();
-                player.UnblockRotation();
-                player.UnblockInspection();
+                LocalPlayer.UnblockMoves();
+                LocalPlayer.UnblockRotation();
+                LocalPlayer.UnblockInspection();
             }
         }
 
@@ -144,9 +175,9 @@ namespace ModShelter
 
         private void InitData()
         {
-            itemsManager = ItemsManager.Get();
-            hUDManager = HUDManager.Get();
-            player = Player.Get();
+            LocalItemsManager = ItemsManager.Get();
+            LocalHUDManager = HUDManager.Get();
+            LocalPlayer = Player.Get();
         }
 
         private void InitSkinUI()
@@ -157,7 +188,43 @@ namespace ModShelter
         private void InitWindow()
         {
             int wid = GetHashCode();
-            ModShelterScreen = GUILayout.Window(wid, ModShelterScreen, InitModShelterScreen, $"{ModName}", GUI.skin.window);
+            ModShelterScreen = GUILayout.Window(wid, ModShelterScreen, InitModShelterScreen, ModName,
+                                                                                        GUI.skin.window,
+                                                                                        GUILayout.ExpandWidth(true),
+                                                                                        GUILayout.MinWidth(ModScreenMinWidth),
+                                                                                        GUILayout.MaxWidth(ModScreenMaxWidth),
+                                                                                        GUILayout.ExpandHeight(true),
+                                                                                        GUILayout.MinHeight(ModScreenMinHeight),
+                                                                                        GUILayout.MaxHeight(ModScreenMaxHeight)
+                                                                                    );
+        }
+
+        private void CollapseWindow()
+        {
+            if (!IsMinimized)
+            {
+                ModShelterScreen = new Rect(ModScreenStartPositionX, ModScreenStartPositionY, ModScreenTotalWidth, ModScreenMinHeight);
+                IsMinimized = true;
+            }
+            else
+            {
+                ModShelterScreen = new Rect(ModScreenStartPositionX, ModScreenStartPositionY, ModScreenTotalWidth, ModScreenTotalHeight);
+                IsMinimized = false;
+            }
+            InitWindow();
+        }
+
+        private void ScreenMenuBox()
+        {
+            if (GUI.Button(new Rect(ModShelterScreen.width - 40f, 0f, 20f, 20f), "-", GUI.skin.button))
+            {
+                CollapseWindow();
+            }
+
+            if (GUI.Button(new Rect(ModShelterScreen.width - 20f, 0f, 20f, 20f), "X", GUI.skin.button))
+            {
+                CloseWindow();
+            }
         }
 
         private void CloseWindow()
@@ -168,44 +235,50 @@ namespace ModShelter
 
         private void InitModShelterScreen(int windowID)
         {
-            using (var verticalScope = new GUILayout.VerticalScope(GUI.skin.box))
+            ModScreenStartPositionX = ModShelterScreen.x;
+            ModScreenStartPositionY = ModShelterScreen.y;
+
+            using (var modContentScope = new GUILayout.VerticalScope(GUI.skin.box))
             {
-                if (GUI.Button(new Rect(430f, 0f, 20f, 20f), "X", GUI.skin.button))
+                ScreenMenuBox();
+                if (!IsMinimized)
                 {
-                    CloseWindow();
+                    ModOptionsBox();
+                    UnlockRestingPlacesBox();
                 }
-
-                using (var horizontalScope = new GUILayout.HorizontalScope(GUI.skin.box))
-                {
-                    GUILayout.Label("Shelter- and bed blueprints.", GUI.skin.label);
-                    if (GUILayout.Button("Unlock resting places", GUI.skin.button))
-                    {
-                        OnClickUnlockRestingPlacesButton();
-                        CloseWindow();
-                    }
-                }
-
-                InstantFinishConstructionsOptionButton();
             }
             GUI.DragWindow(new Rect(0f, 0f, 10000f, 10000f));
         }
 
-        private void InstantFinishConstructionsOptionButton()
+        private void ModOptionsBox()
         {
             if (IsModActiveForSingleplayer || IsModActiveForMultiplayer)
             {
                 using (var horizontalScope = new GUILayout.HorizontalScope(GUI.skin.box))
                 {
-                    InstantFinishConstructionsOption = GUILayout.Toggle(InstantFinishConstructionsOption, $"Use F8 to instantly finish any constructions?", GUI.skin.toggle);
+                    InstantFinishConstructionsOption = GUILayout.Toggle(InstantFinishConstructionsOption, $"Use [F8] to instantly finish any constructions?", GUI.skin.toggle);
                 }
             }
             else
             {
-                using (var verticalScope = new GUILayout.VerticalScope(GUI.skin.box))
+                using (var infoScope = new GUILayout.VerticalScope(GUI.skin.box))
                 {
-                    GUILayout.Label("Use F8 to instantly finish any constructions", GUI.skin.label);
-                    GUILayout.Label("is only for single player or when host", GUI.skin.label);
-                    GUILayout.Label("Host can activate using ModManager.", GUI.skin.label);
+                    GUI.color = Color.yellow;
+                    GUILayout.Label(OnlyForSinglePlayerOrHostMessage(), GUI.skin.label);
+                    GUI.color = Color.white;
+                }
+            }
+        }
+
+        private void UnlockRestingPlacesBox()
+        {
+            using (var horizontalScope = new GUILayout.HorizontalScope(GUI.skin.box))
+            {
+                GUILayout.Label("Click to unlock all shelter - and bed info: ", GUI.skin.label);
+                if (GUILayout.Button("Unlock blueprints", GUI.skin.button))
+                {
+                    OnClickUnlockRestingPlacesButton();
+                    CloseWindow();
                 }
             }
         }
@@ -218,8 +291,15 @@ namespace ModShelter
             }
             catch (Exception exc)
             {
-                ModAPI.Log.Write($"[{ModName}.{ModName}:{nameof(OnClickUnlockRestingPlacesButton)}] throws exception: {exc.Message}");
+                HandleException(exc, nameof(OnClickUnlockRestingPlacesButton));
             }
+        }
+
+        private void HandleException(Exception exc, string methodName)
+        {
+            string info = $"[{ModName}:{methodName}] throws exception:\n{exc.Message}";
+            ModAPI.Log.Write(info);
+            ShowHUDBigInfo(HUDBigInfoMessage(info, MessageType.Error, Color.red));
         }
 
         public void UnlockAllRestingPlaces()
@@ -228,32 +308,24 @@ namespace ModShelter
             {
                 if (!HasUnlockedRestingPlaces)
                 {
-                    RestingPlaceItemInfos = itemsManager.GetAllInfos().Values.Where(info => info.IsShelter()).ToList();
-
                     UnlockShelters();
-
                     UnlockBeds();
-
                     foreach (ItemInfo restingPlaceItemInfo in RestingPlaceItemInfos)
                     {
-                        itemsManager.UnlockItemInNotepad(restingPlaceItemInfo.m_ID);
-                        itemsManager.UnlockItemInfo(restingPlaceItemInfo.m_ID.ToString());
+                        LocalItemsManager.UnlockItemInNotepad(restingPlaceItemInfo.m_ID);
+                        LocalItemsManager.UnlockItemInfo(restingPlaceItemInfo.m_ID.ToString());
                         ShowHUDInfoLog(restingPlaceItemInfo.m_ID.ToString(), "HUD_InfoLog_NewEntry");
                     }
                     HasUnlockedRestingPlaces = true;
                 }
                 else
                 {
-                    ShowHUDBigInfo(
-                         HUDBigInfoMessage(
-                             $"<color=#{ColorUtility.ToHtmlStringRGBA(Color.yellow)}>All resting places were already unlocked!</color>"),
-                        $"{ModName} Info",
-                        HUDInfoLogTextureType.Count.ToString());
+                    ShowHUDBigInfo(HUDBigInfoMessage(AlreadyUnlockedBlueprints(), MessageType.Warning, Color.yellow));
                 }
             }
             catch (Exception exc)
             {
-                ModAPI.Log.Write($"[{ModName}.{ModName}:{nameof(UnlockAllRestingPlaces)}] throws exception: {exc.Message}");
+                HandleException(exc, nameof(UnlockAllRestingPlaces));
             }
         }
 
@@ -261,7 +333,7 @@ namespace ModShelter
         {
             foreach (ItemID shelterItemId in ShelterItemIds)
             {
-                ItemInfo shelterInfo = itemsManager.GetInfo(shelterItemId);
+                ItemInfo shelterInfo = LocalItemsManager.GetInfo(shelterItemId);
                 if (!RestingPlaceItemInfos.Contains(shelterInfo))
                 {
                     RestingPlaceItemInfos.Add(shelterInfo);
@@ -273,7 +345,7 @@ namespace ModShelter
         {
             foreach (ItemID bedItemId in BedItemIds)
             {
-                ItemInfo bedInfo = itemsManager.GetInfo(bedItemId);
+                ItemInfo bedInfo = LocalItemsManager.GetInfo(bedItemId);
                 if (!RestingPlaceItemInfos.Contains(bedInfo))
                 {
                     RestingPlaceItemInfos.Add(bedInfo);
