5c78cf481cafbf26da38b458e0186438d63c052f
Immanuel Van Meirhaeghe <immanuelvm@live.be>
2020-12-18 18:41:04 +0100

    tt

---
+++ Extensions/ItemInfoExtended.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModShelter.Extensions
+{
+    class ItemInfoExtended : ItemInfo
+    {
+        /// <summary>
+        /// Replacing existing with mine
+        /// </summary>
+        /// <returns></returns>
+        public new bool IsShelter()
+        {
+            return true;
+        }
+
+    }
+}
