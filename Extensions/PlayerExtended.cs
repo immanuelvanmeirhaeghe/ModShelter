778dcc14507098c1fd7ba57b320c6e62637d8fb9
Immanuel Van Meirhaeghe <immanuelvm@live.be>
2023-04-27 05:32:16 +0200

    13055968449849

--- Extensions/PlayerExtended.cs
+++ Extensions/PlayerExtended.cs
@@ -10,6 +10,7 @@ namespace ModShelter.Extensions
             base.Start();
             new GameObject($"__{nameof(ModShelter)}__").AddComponent<ModShelter>();
             new GameObject($"__{nameof(ConstructionsManager)}__").AddComponent<ConstructionsManager>();
+            new GameObject($"__{nameof(StylingManager)}__").AddComponent<StylingManager>();
         }
     }
 }
